954f79131c015776c5635561b51e75c78f8ac998
ofir <davidof@sheridancollege.ca>
2021-08-25 15:36:00 -0400

    changed the back button

--- pages/RecipeDetails.js
+++ pages/RecipeDetails.js
@@ -1,7 +1,6 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState,useEffect,Component } from 'react';
 import { Text, Button, View, StyleSheet, TouchableOpacity, SafeAreaView, ImageBackground } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
+import { Icon } from 'react-native-elements';
 export default function RecipeDetails({route, navigation}){
     let [recipeData, setRecipeData] = useState([]);
     return(
@@ -9,14 +8,14 @@ export default function RecipeDetails({route, navigation}){
             <View style={style.insideView}>
                 <ImageBackground source={{ uri: "https://www.inspiredtaste.net/wp-content/uploads/2018/12/Easy-Pasta-Salad-Recipe-3-1200.jpg" }} style={{flex: 4}}>
                     <TouchableOpacity style={style.backButton} onPress={() => navigation.goBack()}>
-                        <Text>Back</Text>
+                        <Text style={{alignSelf:"center",padding:5}}><Icon name='arrow-back' type='material' size={20}/></Text>
                     </TouchableOpacity>  
                 </ImageBackground>
                 <View style={style.textViewHolder}>
                     <View style={style.textView}>
                     <Text style={{ fontSize: 30 }}>{route.params.data.title}</Text>
-                    <Text>{route.params.data.cooktime}</Text>
-                    <Text>{route.params.data.rating}</Text>
+                    <Text>Time: {route.params.data.cooktime}</Text>
+                    <Text>Rating: {route.params.data.rating}</Text>
                     </View>
                 </View> 
             </View>
@@ -39,9 +38,8 @@ const style = StyleSheet.create({
         margin: 20
     },
     backButton: {
-        width: 60,
-        height: 40,
-        backgroundColor: "#FFFFFFBB",
+        width: "8%",
+        height: "auto",
         borderRadius: 10,
         margin: 20,
         marginTop: 50
